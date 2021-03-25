ff5423de06abf597bf6a6edb5557cba981b38ac1
fridavbg <fridavbg@gmail.com>
2021-03-25 15:05:26 +0100

    basic setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'App.css';
+import img from './img/Frida.jpg';
+
+const App = () => 
+(
+    <div className="test">
+        <h1>Hello React</h1>
+        <img src={img} />
+    </div>
+)
+
+export default App;
